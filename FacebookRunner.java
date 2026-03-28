0cc8a685a65e47b1815258b507efb75283472765
Dharanendra21 <dhanushobha.30@gmail.com>
2026-03-28 12:50:24 +0530

    task 28-3-26

---
+++ FacebookRunner.java
@@ -0,0 +1,16 @@
+class FacebookRunner{
+	
+	public static void main(String[]args)
+	{
+		System.out.println("-------First time method Call--------");
+		String user1 =Facebook.validateAndRegister(8430988576l,"gow@gmail.com","20-02-2004","user1","gowtham_20");
+		System.out.println(user1);
+		
+		System.out.println("-------Second time method Call--------");
+		String user2 =Facebook.validateAndRegister(8497988576l,"Dhanu@gmail.com","15-07-2004","user2","Dhanu_15");
+		System.out.println(user2);
+		
+	}
+
+
+}

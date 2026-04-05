06efdd46be3df98c3d244b9f091e21e4b42210d9
Dharanendra21 <dhanushobha.30@gmail.com>
2026-04-05 14:37:33 +0530

    test

--- Puma.java
+++ Puma.java
@@ -1,16 +1,4 @@
 class Puma{
-	double shirtPrice;
-	String shirtcolor;
-	
-	public Puma(){}
-	public Puma(double shirtPrice,String shirtcolor){
-		this.shirtPrice=shirtPrice;
-		this.shirtcolor=shirtcolor;
-		
-	}
-	public void display(){
-		System.out.println("shirtPrice"+shirtPrice);
-		System.out.println("shirtcolor"+shirtcolor);
-	}
+
 
 }
--- PumaRunner.java
+++ PumaRunner.java
@@ -1,6 +1,5 @@
-class PumaRunner{
-	public static void main(String[] args){
-		Puma ref=new Puma(1299.00,"Black");
-		ref.display();
-	}
+class  PumaRunner{
+
+
+
 }
